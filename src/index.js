b8b3dd0e4b2eaee35d29b072bd0659522b217ad5
VK <vitaliy.krynytskyy@gmail.com>
2019-11-12 17:00:15 +0000

    Used redux to make user state available globally

--- src/index.js
+++ src/index.js
@@ -6,20 +6,30 @@ import {
   Route,
   withRouter
 } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import App from "./components/App";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 
 import firebase from "./firebase";
+import rootReducer from "./reducers";
+import { setUser } from "./actions";
+
 import "semantic-ui-css/semantic.min.css";
 
 import * as serviceWorker from "./serviceWorker";
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 class Root extends React.Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
+        //console.log(user);
+        this.props.setUser(user);
         this.props.history.push("/");
       }
     });
@@ -36,12 +46,18 @@ class Root extends React.Component {
   }
 }
 
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(
+  connect(null, {
+    setUser
+  })(Root)
+);
 
 ReactDOM.render(
-  <Router>
-    <RootWithAuth />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
 
