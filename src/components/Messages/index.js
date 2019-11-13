f8ef15fa1a675865347f739f08721e6df2515853
VK <vitaliy.krynytskyy@gmail.com>
2019-11-13 15:48:21 +0000

    Added makrup for the messages section

--- src/components/Messages/index.js
+++ src/components/Messages/index.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { Segment, Comment } from "semantic-ui-react";
+
+import MessageForm from "../MessagesForm";
+import MessagesHeader from '../MessagesHeader';
 
 class Messages extends React.Component {
   render() {
-    return <div>Messages go here</div>;
+    return (
+      <React.Fragment>
+        <MessagesHeader />
+        <Segment>
+          <Comment.Group className="messages"></Comment.Group>
+        </Segment>
+        <MessageForm />
+      </React.Fragment>
+    );
   }
 }
 
