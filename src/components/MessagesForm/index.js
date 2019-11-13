2d26f5d263816cffabbd7caf7eb91c114230b52b
VK <vitaliy.krynytskyy@gmail.com>
2019-11-13 19:39:15 +0000

    Added a modal popup for uploading media files

--- src/components/MessagesForm/index.js
+++ src/components/MessagesForm/index.js
@@ -3,14 +3,20 @@ import { Segment, Button, Input } from "semantic-ui-react";
 
 import firebase from "../../firebase";
 
+import FileModal from "../FileModal";
+
 class MessageForm extends React.Component {
   state = {
     message: "",
     user: this.props.currentUser,
     channel: this.props.currentChannel,
     errors: [],
-    loading: false
+    loading: false,
+    modal: false
   };
+
+  openModal = () => this.setState({ modal: true });
+  closeModal = () => this.setState({ modal: false });
 
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
@@ -57,7 +63,7 @@ class MessageForm extends React.Component {
   };
 
   render() {
-    const { errors, message, loading } = this.state;
+    const { errors, message, loading, modal } = this.state;
 
     return (
       <Segment className="message__form">
@@ -88,10 +94,12 @@ class MessageForm extends React.Component {
           />
           <Button
             color="teal"
+            onClick={this.openModal}
             content="Upload Media"
             labelPosition="right"
             icon="cloud upload"
           />
+          <FileModal modal={modal} closeModal={this.closeModal} />
         </Button.Group>
       </Segment>
     );
