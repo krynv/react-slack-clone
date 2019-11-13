213178be45c82d0fe587e1ae794d30336bf77233
VK <vitaliy.krynytskyy@gmail.com>
2019-11-13 08:58:29 +0000

    Added styling to the left size panel component

--- src/components/SidePanel/index.js
+++ src/components/SidePanel/index.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { Menu } from "semantic-ui-react";
 
 class SidePanel extends React.Component {
   render() {
-    return <div>Side Panel</div>;
+    return (
+      <Menu
+        size="large"
+        inverted
+        fixed="left"
+        vertical
+        style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
+      ></Menu>
+    );
   }
 }
 
