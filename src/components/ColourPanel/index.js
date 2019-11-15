b9b30b847bc0861524868a662a3ee04d73538ff4
VK <vitaliy.krynytskyy@gmail.com>
2019-11-15 13:26:36 +0000

    Added a modal panel for changing the default app colours
    - WIP changing app colours

--- src/components/ColourPanel/index.js
+++ src/components/ColourPanel/index.js
@@ -1,8 +1,25 @@
 import React from "react";
-import { Sidebar, Menu, Divider, Button } from "semantic-ui-react";
+import {
+  Sidebar,
+  Menu,
+  Divider,
+  Button,
+  Modal,
+  Icon,
+  Label
+} from "semantic-ui-react";
 
 class ColourPanel extends React.Component {
+  state = {
+    modal: false
+  };
+
+  openModal = () => this.setState({ modal: true });
+  closeModal = () => this.setState({ modal: false });
+
   render() {
+    const { modal } = this.state;
+
     return (
       <Sidebar
         as={Menu}
@@ -13,7 +30,22 @@ class ColourPanel extends React.Component {
         width="very thin"
       >
         <Divider />
-        <Button icon="add" size="small" color="blue" />
+        <Button icon="add" size="small" color="blue" onClick={this.openModal} />
+
+        <Modal basic open={modal} onClose={this.closeModal}>
+          <Modal.Header>Choose your preferred colours</Modal.Header>
+
+          <Modal.Content></Modal.Content>
+
+          <Modal.Actions>
+            <Button color="green" inverted>
+              <Icon name="checkmark" /> Save Changes
+            </Button>
+            <Button color="red" inverted>
+              <Icon name="remove" /> Cancel
+            </Button>
+          </Modal.Actions>
+        </Modal>
       </Sidebar>
     );
   }
