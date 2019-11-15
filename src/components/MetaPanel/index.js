c000c1cc771a8130b933e219161713865552deb0
VK <vitaliy.krynytskyy@gmail.com>
2019-11-15 11:59:46 +0000

    Added an accordion to the meta panel

--- src/components/MetaPanel/index.js
+++ src/components/MetaPanel/index.js
@@ -1,8 +1,69 @@
 import React from "react";
+import { Segement, Accordion, Header, Icon, Segment } from "semantic-ui-react";
 
 class MetaPanel extends React.Component {
+  state = {
+    activeIndex: 0
+  };
+
+  setActiveIndex = (e, titleProps) => {
+    const { index } = titleProps;
+    const { activeIndex } = this.state;
+    const newIndex = activeIndex === index ? -1 : index;
+
+    this.setState({ activeIndex: newIndex });
+  };
+
   render() {
-    return <div>Meta information goes here</div>;
+    const { activeIndex } = this.state;
+
+    return (
+      <Segment>
+        <Header as="h3" attached="top">
+          About Channel
+        </Header>
+        <Accordion styled attached="true">
+          <Accordion.Title
+            active={activeIndex === 0}
+            index={0}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name="dropdown" />
+            <Icon name="info" />
+            Channel Details
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 0}>
+            [details]
+          </Accordion.Content>
+
+          <Accordion.Title
+            active={activeIndex === 1}
+            index={1}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name="dropdown" />
+            <Icon name="user circle" />
+            Top Contributors
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 1}>
+            [users]
+          </Accordion.Content>
+
+          <Accordion.Title
+            active={activeIndex === 2}
+            index={2}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name="dropdown" />
+            <Icon name="pencil alternate" />
+            Channel created by:
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 2}>
+            [user]
+          </Accordion.Content>
+        </Accordion>
+      </Segment>
+    );
   }
 }
 
